0d12ead57439588cfc7987007c720bce9c554f9d
juandnn <j.niebles@uniandes.edu.co>
2023-08-21 21:30:15 -0500

    corrección jump y avances oneValueArg

--- parser_1.py
+++ parser_1.py
@@ -35,7 +35,7 @@ def jump(command_jump:str)-> bool:
     """ 
     lo ideal es que reciba un 'jump(valor_1 : int, valor_2 : int)' sin valores de espacio al rededor.
     
-    si taka lo quiere romper, no creo que lo pueda romper * felicidad
+    si taka lo quiere romper creo que no lo puede romper
     
     """ 
     
@@ -71,7 +71,7 @@ def jump(command_jump:str)-> bool:
                 if possible_argument:
                     print("XD")
                     
-                    while num.count("(") == num.count(")") and num.count("(") > 0 :
+                    while num.count("(") == num.count(")") and num.count("(") > 0 and auxiliarParentesis(num):
                         num = num[1 :-1]
                         print("XDXD")
                     
@@ -89,7 +89,11 @@ def jump(command_jump:str)-> bool:
 #print(jump("jump  (   (12),   13  )" ))
 
 def nop(command_nop:str) -> bool:
-    
+    """
+    nop( "no p() ") -> false
+    nop( "    nop   () " ) -> true
+
+    """
     filtered_command = ""
     
     if "nop" in command_nop:
@@ -106,9 +110,55 @@ def nop(command_nop:str) -> bool:
 
 
 
-def oneValueArg()->bool:
-    return 
+def oneValueArg(one_value_arg_command:str):
+    
+    one_value_arg_command = one_value_arg_command.rstrip(" ").lstrip(" ")
+    
+    works = True
+    
+    # condicion 1
+    index1 = str(one_value_arg_command).rfind("(")
+    works = works and True if index1 != -1 else works and False # si no hay "(" está mal escrito
+    
+    # condicion 2 - palabra permitida
+    base_command = one_value_arg_command[:index1].lstrip(" ")
+    possible_base_command = ["walk", "leap", "drop","get", "grab", "letgo","turn","turnto"]
+    works = works and True if base_command in possible_base_command else works and False
+    
+    # condicion 3 - parametros correctos 
+    
+    base_argument = one_value_arg_command.rstrip(base_command).rstrip(" ")
+    
+    if base_command not in ["turn", "turnto"]:
+        try:
+            while (base_command.count("(") > 0) and (base_command.count("(") == base_command.count(")")):
+                base_command.rstrip("(").rsplit(")")
+                break
+            
+            
+            
+        except:
+            print("a llorar")
+    
+    
+    
+    
+    
+    return False
 
 """print("Hola taiwanes")
 print("arabia saudita")"""
+
+def auxiliarParentesis(stringconparentesis:str) -> bool:
+    """ revisa que los '(' se cierren de manera correcta, solo funciona para cada único par """
+    
+    index1 = stringconparentesis.find("(")
+    index2 = stringconparentesis.find(")")
+    
+    works = True
+    if index1 > index2:
+        works = False
+
+    return works
+
 
