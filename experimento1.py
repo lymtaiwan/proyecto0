306fffa6bc77dba8f39c7c9b8f0e51da3e137446
juandnn <j.niebles@uniandes.edu.co>
2023-09-03 19:56:06 -0500

    quiero amor y un besito

--- experimento1.py
+++ experimento1.py
@@ -1,8 +1,9 @@
-def quieroAmor():
+def quieroAmor(a):
+    print(a)
     return True
 
 espacio_vectorial = {}
 
 espacio_vectorial["uno"] = quieroAmor
 
-print(espacio_vectorial["uno"]())
+print(espacio_vectorial["uno"]("me das un besito? uwu"))
